066909ca3b4cbcd3a0b66cab50d27435350e4450
kunalanvithushan <kunalanvithusan@gmail.com>
2025-01-11 20:18:03 +0530

    temp

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import "./App.css";
 import Header from "./components/Header/Header";
 import Home from "./pages/display/Home/Home";
 import Footer from "./components/Footer/footer";
-import PopupBasket1 from "./components/popupBasket/popupBasket1.jsx"
+//import PopupBasket1 from "./popupBasket/popupBasket1.jsx"
 import "bootstrap/dist/css/bootstrap.min.css";
 
 import { Routes, Route, Link } from "react-router-dom";
@@ -13,14 +13,14 @@ import { Routes, Route, Link } from "react-router-dom";
 function App() {
   return (
     <>
-      {/* <Header />
+      <Header />
       <div div className="main-container" style={{ paddingTop: "100px" }}>
         <section id="Home">
          <Home /> 
         </section>
       </div>
-      <Footer /> */}
-     <PopupBasket1/>
+      <Footer />
+     
 
     </>
   );
